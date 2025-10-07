c611b25ac1dee4f893eba44937fee1c8e5daa772
Patel Neev <pneev32@gmail.com>
2025-10-07 23:23:56 +0530

    Update ass_1034_q1.c

--- ass_1034_q1.c
+++ ass_1034_q1.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 void main()
 {
-int a,sum=0,b;
-printf("Enter a value of a ");
-scanf("%d",&a);
-while(a>0)
- {
-   b = a % 10 ; // Extract the last digit of the number
-   sum = sum + b ; // Add the extracted digit to 'sum'
-   a = a / 10 ; // Remove the last digit from 'a'
- } 
-printf("\n Sum of numbers = %d",sum);
+  int a,sum=0,b;
+  printf("Enter a value of a ");
+  scanf("%d",&a);
+   while(a>0)
+     {
+        b = a % 10 ; // Extract the last digit of the number
+        sum = sum + b ; // Add the extracted digit to 'sum'
+        a = a / 10 ; // Remove the last digit from 'a'
+     } 
+   printf("\n Sum of numbers = %d",sum);
 }
 
