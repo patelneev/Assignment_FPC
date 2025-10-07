d669cca4b687d089e6c92883db7a1226d8860263
Patel Neev <pneev32@gmail.com>
2025-10-07 23:29:08 +0530

    Update ass_1034_q11.c

--- ass_1034_q11.c
+++ ass_1034_q11.c
@@ -2,32 +2,32 @@
 #include <stdio.h>
 void main()
 {
-int a[3][3], b[3][3], sum[3][3];
-int i, j, r, c;
-printf("\n Enter first matrix \n");
-for(i = 0; i <= 2; i++)
-{
-for(j = 0; j <= 2; j++)
-{
-printf(" Enter a no");
-scanf("%d", &a[i][j]);
-}
-}
-printf("\n Enter second matrix:\n");
-for(i = 0; i <= 2; i++)
-{
-for(j = 0; j <= 2; j++)
-{
-printf(" Enter a no");
-scanf("%d", &b[i][j]);
-}
-}
-for(i = 0; i <= 2; i++)
-{
-for(j = 0; j <= 2; j++)
-{
-printf(" %d ",sum[i][j] = a[i][j] + b[i][j]); //Addition Formula
-}
-printf("\n");
-}
+  int a[3][3], b[3][3], sum[3][3];
+  int i, j, r, c;
+  printf("\n Enter first matrix \n");
+  for(i = 0; i <= 2; i++)
+    {
+      for(j = 0; j <= 2; j++)
+       {
+         printf(" Enter a no");
+         scanf("%d", &a[i][j]);
+       }
+    }
+    printf("\n Enter second matrix:\n");
+    for(i = 0; i <= 2; i++)
+      {
+         for(j = 0; j <= 2; j++)
+            {
+              printf(" Enter a no");
+              scanf("%d", &b[i][j]);
+             }
+      }
+    for(i = 0; i <= 2; i++)
+      {
+         for(j = 0; j <= 2; j++) 
+           {
+             printf(" %d ",sum[i][j] = a[i][j] + b[i][j]); //Addition Formula
+           }
+        printf("\n");
+      }
 }
