643c12b26718839445326a3d703e27c8fe4757d4
Patel Neev <pneev32@gmail.com>
2025-10-07 23:48:01 +0530

    Update ass_1034_q6.c

--- ass_1034_q6.c
+++ ass_1034_q6.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 void main()
 {
-int a, b;
-printf("\n Enter two numbers: ");
-scanf("%d ",&a);
-printf("\n Enter two numbers: ");
-scanf("%d ",&b);
-while(a != b)
-{
-if(a > b)
-{
-a=a-b; // Subtract smaller from larger
-}
-else
-{
-b=b-a;
-}
-}
-printf("GCD = %d", a);
+  int a, b;
+  printf("\n Enter two numbers: ");
+  scanf("%d ",&a);
+  printf("\n Enter two numbers: ");
+  scanf("%d ",&b);
+  while(a != b)
+   {
+    if(a > b)
+      {
+        a=a-b; // Subtract smaller from larger
+      }
+    else
+      {
+        b=b-a;
+      }
+    }
+  printf("GCD = %d", a);
 }
