d2831836aad204847d134521998ca4c09e974c40
Patel Neev <pneev32@gmail.com>
2025-10-07 23:37:38 +0530

    Update ass_1034_q14.c

--- ass_1034_q14.c
+++ ass_1034_q14.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 void main()
 {
-int l,b,p;
-printf("Enter length of rectangle: ");
-scanf("%d", &l);
-printf("Enter breadth of rectangle: ");
-scanf("%d", &b);
-p = 2 * (l+b); // Formula for perimeter
-printf("\n Perimeter of rectangle = %d \n", p);
+  int l,b,p;
+  printf("Enter length of rectangle: ");
+  scanf("%d", &l);
+  printf("Enter breadth of rectangle: ");
+  scanf("%d", &b);
+     p = 2 * (l+b); // Formula for perimeter
+  printf("\n Perimeter of rectangle = %d \n", p);
 }
