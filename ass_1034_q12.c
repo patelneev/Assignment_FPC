8baf64ffab00d871d359dae2f341e72ca23267d8
Patel Neev <pneev32@gmail.com>
2025-10-07 23:29:44 +0530

    Update ass_1034_q12.c

--- ass_1034_q12.c
+++ ass_1034_q12.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 void main()
 {
-int n, i;
-printf("Enter a number N: ");
-scanf("%d", &n);
-for(i = n; i >= 1; i--)
-{
-printf("%d \n", i);
-}
+   int n, i;
+   printf("Enter a number N: ");
+   scanf("%d", &n);
+   for(i = n; i >= 1; i--)
+     {
+        printf("%d \n", i);
+     }
 }
