61068878a66b4465cd1ad4e478bba34c5b30d43f
Patel Neev <pneev32@gmail.com>
2025-10-08 10:00:24 +0530

    Update ass_1034_q3.c

--- ass_1034_q3.c
+++ ass_1034_q3.c
@@ -10,7 +10,7 @@ void main()
     for (int i = 0; str[i] != '\0'; i++)
      {
             if (!(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ||
-                  str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')) 
+ str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')) 
           {
             c++;
           }
