355b3d682043d10244469bca9246f4ee6d61895c
Patel Neev <pneev32@gmail.com>
2025-10-08 09:59:40 +0530

    Update ass_1034_q3.c

--- ass_1034_q3.c
+++ ass_1034_q3.c
@@ -9,8 +9,8 @@ void main()
   fgets(str, sizeof(str), stdin); // Read string input (including spaces)
     for (int i = 0; str[i] != '\0'; i++)
      {
-       if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A'
-           || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            if (!(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ||
+                  str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')) 
           {
             c++;
           }
